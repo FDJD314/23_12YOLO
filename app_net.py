5282914dba24c7ad336a2c1eb6069a0302002bb4
fdjd <2831599729@qq.com>
2023-12-27 15:13:53 +0800

    删除Colos类

--- app_net.py
+++ app_net.py
@@ -7,47 +7,10 @@ import torch
 from ultralytics.utils import ops
 
 
-class Colors:
-    def __init__(self):
-        """Initialize colors as hex = matplotlib.colors.TABLEAU_COLORS.values()."""
-        hexs = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
-                '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
-        self.palette = [self.hex2rgb(f'#{c}') for c in hexs]
-        self.n = len(self.palette)
-        self.pose_palette = np.array([[255, 128, 0], [255, 153, 51], [255, 178, 102], [230, 230, 0], [255, 153, 255],
-                                      [153, 204, 255], [255, 102, 255], [255, 51, 255], [102, 178, 255], [51, 153, 255],
-                                      [255, 153, 153], [255, 102, 102], [255, 51, 51], [153, 255, 153], [102, 255, 102],
-                                      [51, 255, 51], [0, 255, 0], [0, 0, 255], [255, 0, 0], [255, 255, 255]],
-                                     dtype=np.uint8)
-
-    def __call__(self, i, bgr=False):
-        """Converts hex color codes to RGB values."""
-        c = self.palette[int(i) % self.n]
-        return (c[2], c[1], c[0]) if bgr else c
-
-    @staticmethod
-    def hex2rgb(h):
-        """Converts hex color codes to RGB values (i.e. default PIL order)."""
-        return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
-
-
-colors = Colors()  # create instance for 'from utils.plots import colors'
-
-
 class MyNet:
     def __init__(self, modelfile: Union[str, Path]):
-        self.dnn = False
-        self.data = 'coco.yaml'
-        self.half = False
         self.device = 'cpu'
-        verbose = False
         self._lock = threading.Lock()
-        self.conf = 0.25
-        self.iou = 0.7
-        self.agnostic_nms = False
-        self.max_det = 300
-        self.classes = None
-        self.path = None
         self.imgsz = [640, 640]
 
         self.model = torch.load(modelfile)
@@ -56,24 +19,22 @@ class MyNet:
         # self.warmup()
 
     def __call__(self, data=None):
-        self.path = data["path"]
+        path = data["path"]
         if data["mode"] == "image":
-            img_r = self.run(cv.imread(self.path))
+            img_r = self.run(cv.imread(path))
             cv.imwrite("tmp/results.jpg", img_r)
         elif data["mode"] == "video":
-            # self.run()
-
             cap = cv.VideoCapture(data["path"])
             fourcc, fps = cap.get(cv.CAP_PROP_FOURCC), cap.get(cv.CAP_PROP_FPS)
             h, w = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
             fourcc = cv.VideoWriter.fourcc(*"mp4v")
-            video = cv.VideoWriter("results.mp4", fourcc, fps, (w, h))
+            video = cv.VideoWriter("tmp/results.mp4", fourcc, fps, (w, h))
 
             while cap.isOpened():
                 success, frame = cap.read()
                 if not success:
                     break
-                img_r = self.run(frame)[0].plot()
+                img_r = self.run(frame)
                 video.write(img_r)
 
             cap.release()
@@ -96,18 +57,19 @@ class MyNet:
         r = min(self.imgsz[0] / c_shape[0], self.imgsz[1] / c_shape[1])
 
         new_unpad = int(round(c_shape[1] * r)), int(round(c_shape[0] * r))
-        # dw, dh = self.imgsz[1] - new_unpad[1], self.imgsz[0] - new_unpad[0]  # wh padding
-        # dw, dh = np.mod(dw, 32)/2, np.mod(dh, 32)/2  # wh padding
+        dw, dh = self.imgsz[0] - new_unpad[0], self.imgsz[1] - new_unpad[1]  # wh padding
+        dw, dh = np.mod(dw, 32) / 2, np.mod(dh, 32) / 2  # wh padding
 
         if c_shape[::-1] != new_unpad:  # resize
             img = cv.resize(img, new_unpad, interpolation=cv.INTER_LINEAR)
-        # top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-        # left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-        # img = cv.copyMakeBorder(img, top, bottom, left, right, cv.BORDER_CONSTANT,
-        #                         value=(114, 114, 114))  # add border
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        img = cv.copyMakeBorder(img, top, bottom, left, right, cv.BORDER_CONSTANT,
+                                value=(114, 114, 114))  # add border
         return img
 
-    def scale_boxes(self, img1_shape, boxes, img0_shape):
+    @staticmethod
+    def scale_boxes(img1_shape, boxes, img0_shape):
         gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
         pad = round((img1_shape[1] - img0_shape[1] * gain) / 2 - 0.1), round(
             (img1_shape[0] - img0_shape[0] * gain) / 2 - 0.1)  # wh padding
@@ -138,22 +100,10 @@ class MyNet:
         return torch.unsqueeze(im, dim=0)
 
     def postprocess(self, preds, img, orig_img):
-        pred = ops.non_max_suppression(preds,
-                                       self.conf,
-                                       self.iou,
-                                       agnostic=self.agnostic_nms,
-                                       max_det=self.max_det,
-                                       classes=self.classes)[0]
+        pred = ops.non_max_suppression(preds, 0.6, 0.7, agnostic=False, max_det=300, classes=None)[0]
 
         # if not isinstance(orig_img, list):  # input images are a torch.Tensor, not a list
         #     orig_imgs = ops.convert_torch2numpy_batch(orig_img)
-
-        # results = []
-        # for i, pred in enumerate(preds):
-        #     orig_img = orig_imgs[i]
-        #     pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
-        #     results.append(Results(orig_img, path=self.path, names=self.model.names, boxes=pred))
-        # return results
 
         for p in reversed(pred):
             p = self.scale_boxes(img.shape[2:], p, orig_img.shape)
@@ -161,7 +111,7 @@ class MyNet:
             name = self.model.names[c]
             label = f'{name} {conf:.2f}'
 
-            im_c, im_lw = colors(p[5], True), max(round(sum(orig_img.shape) / 2 * 0.003), 2)
+            im_c, im_lw = (0, 255, 127), max(round(sum(orig_img.shape) / 2 * 0.003), 2)
             tf, sf = max(im_lw - 1, 1), im_lw / 3
             p1, p2 = (int(p[0]), int(p[1])), (int(p[2]), int(p[3]))
             cv.rectangle(orig_img, p1, p2, im_c, thickness=im_lw, lineType=cv.LINE_AA)
@@ -182,8 +132,7 @@ class MyNet:
 
 
 if __name__ == '__main__':
-    data = {"mode": "image", "path": "source/test.jpg"}
-    # data = {"mode": "video", "path": "test.mp4"}
-    # net = MyNet('yolov8n.pt')
+    # data = {"mode": "image", "path": "source/test.jpg"}
+    data = {"mode": "video", "path": "source/test.mp4"}
     net = MyNet('model/model.pt')
     net(data)
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import cv2 as cv
 
 if __name__ == '__main__':
     model = YOLO('model/yolov8s.pt')
-    source = 'source/test.jpg'
+    source = 'source/test.mp4'
     results = model(source)
     img_r = results[0].plot()
     cv.imwrite("source/results.jpg", img_r)
