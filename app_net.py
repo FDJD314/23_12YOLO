de1c85d273a9bdc39ce1d4c6e1956484cb5b23af
fdjd <2831599729@qq.com>
2023-12-27 15:48:11 +0800

    项目整理

--- app_net.py
+++ app_net.py
@@ -18,16 +18,15 @@ class MyNet:
         self.model.eval()
         # self.warmup()
 
-    def __call__(self, data=None):
+    def __call__(self, data):
         path = data["path"]
         if data["mode"] == "image":
             img_r = self.run(cv.imread(path))
             cv.imwrite("tmp/results.jpg", img_r)
         elif data["mode"] == "video":
             cap = cv.VideoCapture(data["path"])
-            fourcc, fps = cap.get(cv.CAP_PROP_FOURCC), cap.get(cv.CAP_PROP_FPS)
+            fourcc, fps = cv.VideoWriter.fourcc(*"mp4v"), cap.get(cv.CAP_PROP_FPS)
             h, w = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-            fourcc = cv.VideoWriter.fourcc(*"mp4v")
             video = cv.VideoWriter("tmp/results.mp4", fourcc, fps, (w, h))
 
             while cap.isOpened():
@@ -85,43 +84,28 @@ class MyNet:
         return boxes
 
     def preprocess(self, im):
-        not_tensor = not isinstance(im, torch.Tensor)
-        if not_tensor:
-            im = self.resize(im)
-            cv.imwrite('tmp.jpg', im)
-            im = im.transpose((2, 0, 1))  # BGR to RGB, BHWC to BCHW, (n, 3, h, w)
-            im = np.ascontiguousarray(im)  # contiguous
-            im = torch.from_numpy(im)
+        im = self.resize(im)
+        im = im.transpose((2, 0, 1))
+        im = np.ascontiguousarray(im)
+        im = torch.from_numpy(im)/255
 
         im = im.to(self.device)
         im = im.float()
-        if not_tensor:
-            im /= 255  # 0 - 255 to 0.0 - 1.0
         return torch.unsqueeze(im, dim=0)
 
     def postprocess(self, preds, img, orig_img):
         pred = ops.non_max_suppression(preds, 0.6, 0.7, agnostic=False, max_det=300, classes=None)[0]
 
-        # if not isinstance(orig_img, list):  # input images are a torch.Tensor, not a list
-        #     orig_imgs = ops.convert_torch2numpy_batch(orig_img)
-
-        for p in reversed(pred):
+        for p in pred:
             p = self.scale_boxes(img.shape[2:], p, orig_img.shape)
-            c, conf = int(p[5]), float(p[4])
-            name = self.model.names[c]
-            label = f'{name} {conf:.2f}'
-
+            pos1, pos2 = (int(p[0]), int(p[1])), (int(p[2]), int(p[3]))
+            label = f'{self.model.names[int(p[5])]} {float(p[4]):.2f}'
             im_c, im_lw = (0, 255, 127), max(round(sum(orig_img.shape) / 2 * 0.003), 2)
             tf, sf = max(im_lw - 1, 1), im_lw / 3
-            p1, p2 = (int(p[0]), int(p[1])), (int(p[2]), int(p[3]))
-            cv.rectangle(orig_img, p1, p2, im_c, thickness=im_lw, lineType=cv.LINE_AA)
 
-            w, h = cv.getTextSize(label, 0, fontScale=sf, thickness=tf)[0]  # text width, height
-            outside = p1[1] - h >= 3
-            p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
-            cv.rectangle(orig_img, p1, p2, im_c, -1, cv.LINE_AA)  # filled
+            cv.rectangle(orig_img, pos1, pos2, im_c, thickness=im_lw, lineType=cv.LINE_AA)
             cv.putText(orig_img,
-                       label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+                       label, (pos1[0], pos1[1]),
                        0,
                        sf,
                        (255, 255, 255),
